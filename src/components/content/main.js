43c2d4af3db447873ac86bf649c9e09d7e49bde7
matheus sousa <matusalem3378@gmail.com>
2023-05-11 18:05:54 -0300

    adicionando seção de matricula

--- src/components/content/main.js
+++ src/components/content/main.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import man from '../../img/man.jpeg';
+//import Plans from "../pages/plans";
+import { Link } from "react-router-dom";
 
 function Main() {
   return (
     <div className='container'>
      <main>
-      <h2>introdução</h2>
-      <img src="" alt="man"/>
+      <h3>introdução</h3>
+      <img src={man} alt="man"/>
      </main>
+     <section className='enroll'>
+      <Link to="plans">Matricule-se</Link>
+     </section>
     </div>
   )
 }
 
-export default Main
+export default Main;
