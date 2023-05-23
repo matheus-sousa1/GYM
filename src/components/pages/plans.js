badfa244d3372e76cbb239a8fb4f954f46aca2ce
matheus sousa <matusalem3378@gmail.com>
2023-05-22 22:19:52 -0300

    iniciando a adição dos planos

--- src/components/pages/plans.js
+++ src/components/pages/plans.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { CgGym } from "react-icons/cg";
+
+
 
 function Plans() {
   return (
-    <div>plans</div>
+    <div className='container-plans'>
+      <CgGym/>
+      <div className='plans'>
+       <h2>PRICE</h2>
+       <ul>
+        <li>beneficio um</li>
+        <li>beneficio 2</li>
+        <li>beneficio 3</li>
+       </ul>
+      </div>
+      <div className='plans'>
+       <h2>PRICE</h2>
+       <ul>
+        <li>beneficio um</li>
+        <li>beneficio 2</li>
+        <li>beneficio 3</li>
+       </ul>
+      </div>
+      <div className='plans'>
+       <h2>PRICE</h2>
+       <ul>
+        <li>beneficio um</li>
+        <li>beneficio 2</li>
+        <li>beneficio 3</li>
+       </ul>
+      </div>
+    </div>
   )
 }
 
