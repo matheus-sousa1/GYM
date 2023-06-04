8145c1921affc6f03e56960d3890f6a42728ec5f
matheus sousa <matusalem3378@gmail.com>
2023-06-04 16:19:37 -0300

    iniciando estrutura de agendamento

--- src/components/pages/appointment.js
+++ src/components/pages/appointment.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Appointment() {
   return (
-    <div>appointment
-      <h1>hand</h1>   
+    <div>
+      <h1>appointment</h1> 
+      <form action="">
+        <label htmlFor="">Nome</label>
+        <input type="text" name="nome" />
+        <label htmlFor="">E-mail</label>
+        <input type="email" name="email" />
+        <input type="date" />
+        <select>
+          <option>Manhã</option>
+          <option>Tarde</option>
+          <option>Noite</option>
+        </select>
+        <textarea></textarea>
+        <button type="submit">Enviar</button>
+      </form>  
     </div>
   )
 }
