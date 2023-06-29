20ed82ea5dbc1c5bfb776c380be8111dc22a8c93
matheus sousa <matusalem3378@gmail.com>
2023-06-28 21:58:30 -0300

    base para agendamento criada

--- src/components/pages/appointment.js
+++ src/components/pages/appointment.js
@@ -1,25 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from "react-hook-form";
+import { useState } from 'react';
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const createUserFormSchema = z.object({
+  name: z.string()
+  .nonempty('o e-mail é obrigatório')
+  .email('o formato do e-mail está inválido'),
+  email:z.string()
+  .min(6, 'a senha precisa de no mínimo 6 caracteres'),
+})
 
 function Appointment() {
+  const [outPut, setOutPut] = useState();
+  const { register, handleSubmit, formState} = useForm(
+   {
+    resolver: zodResolver(createUserFormSchema)
+   }
+  );
+
+  const createUser = (data) => {
+    setOutPut(JSON.stringify(data, null, 2))
+   console.log(data)
+  }
+
   return (
     <div>
       <h1>appointment</h1> 
-      <form action="">
+      <h3>adicione seu E-mail que vamos entrar em contato com você para agendar seu atendimento</h3>
+      <form onSubmit={handleSubmit(createUser)}>
         <label htmlFor="">Nome</label>
-        <input type="text" name="nome" />
+        <input type="text" {...register('name')} />
         <label htmlFor="">E-mail</label>
-        <input type="email" name="email" />
-        <input type="date" />
-        <select>
-          <option>Manhã</option>
-          <option>Tarde</option>
-          <option>Noite</option>
-        </select>
-        <textarea></textarea>
+        <input type="email" {...register('email')} />
         <button type="submit">Enviar</button>
       </form>  
+      <pre>{outPut}</pre>
     </div>
   )
 }
 
-export default Appointment
+export default Appointment;
