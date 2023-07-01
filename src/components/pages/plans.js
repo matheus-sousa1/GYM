0bf3149045996628ee3ed5c4b771ab8c08093f24
matheus sousa <matusalem3378@gmail.com>
2023-07-01 17:58:06 -0300

    finalizado base para planos

--- src/components/pages/plans.js
+++ src/components/pages/plans.js
@@ -8,28 +8,31 @@ function Plans() {
     <div className='container-plans'>
       <CgGym/>
       <div className='plans'>
-       <h2>PRICE</h2>
+       <h2>Plano Básico</h2>
+       <p>Mensalidade</p>
        <ul>
-        <li>beneficio um</li>
-        <li>beneficio 2</li>
-        <li>beneficio 3</li>
+        <li>R$ 55,50 após o 1º mês</li>
+        <li>R$ 80,50 após o 4º mês</li>
        </ul>
+       <button>Assine já</button>
       </div>
       <div className='plans'>
-       <h2>PRICE</h2>
+       <h2>Plano Premium</h2>
+       <p>Mensalidade</p>
        <ul>
-        <li>beneficio um</li>
-        <li>beneficio 2</li>
-        <li>beneficio 3</li>
+        <li>R$ 75,50 após o 1º mês</li>
+        <li>R$ 105,50 após o 4º mês</li>
        </ul>
+       <button>Assine já</button>
       </div>
       <div className='plans'>
-       <h2>PRICE</h2>
+       <h2>Plano Business</h2>
+       <p>Mensalidade</p>
        <ul>
-        <li>beneficio um</li>
-        <li>beneficio 2</li>
-        <li>beneficio 3</li>
+        <li>R$ 90,50 após o 1º mês</li>
+        <li>R$ 125,50 após o 4º mês</li>
        </ul>
+       <button>Assine já</button>
       </div>
     </div>
   )
