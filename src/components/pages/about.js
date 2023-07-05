70a599c50613a854ed3e0234c13e20e50ec5b1d4
matheus sousa <matusalem3378@gmail.com>
2023-07-05 17:28:52 -0300

    sobre base feita

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function About() {
   return (
-    <div>about</div>
+    <div>
+      <div className='text'>
+       <h1>About us</h1>
+       <p>
+       </p>
+      </div>
+      <img src=""></img>
+    </div>
   )
 }
 
