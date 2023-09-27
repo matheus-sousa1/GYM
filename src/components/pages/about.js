a509bc823e3175000d0e8dd1d5809861ad1bc4b7
matheus sousa <matusalem3378@gmail.com>
2023-09-27 18:54:41 -0300

    página sobre feita

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import imageAbout from '../../img/about.jpg';
+import "./about.css";
 
 function About() {
   return (
-    <div>
+    <div className='container-about'>
       <div className='text'>
-       <h1>About us</h1>
+       <h1>Sobre nós</h1>
        <p>
+       Na Academia GYM, nossa missão é mais do que apenas construir músculos. Estamos comprometidos em transformar vidas, promovendo saúde, bem-estar e autoestima. Há anos, temos sido o destino preferido por aqueles que desejam alcançar seus objetivos de condicionamento físico e adotar um estilo de vida mais ativo. Localizada no coração da cidade, nossa academia é muito mais do que apenas um espaço repleto de máquinas de exercícios. É um lugar onde os sonhos se tornam realidade, onde pessoas de todas as idades, tamanhos e níveis de encontrar se reúnem para se superar e se tornarem a melhor versão de si mesmas.
        </p>
       </div>
-      <img src=""></img>
+      <img src={imageAbout} />
+      <footer>
+      <h4>Todos os direitos reservados</h4>
+      </footer>
     </div>
   )
 }
