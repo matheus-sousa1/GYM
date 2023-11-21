34b63623d408b02dc1cd3fd2e97ac5752d49c7e3
matheus sousa <matusalem3378@gmail.com>
2023-11-21 17:23:32 -0300

    responsividade da barra de navegação feita

--- src/components/pages/navBar.js
+++ src/components/pages/navBar.js
@@ -8,12 +8,23 @@ function NavBar() {
       <h1>
         Gym
       </h1>
+      <input type="checkbox" id="checkbox_toggle" />
+ <label for="checkbox_toggle" className="hamburger">&#9776;</label>
       <nav>
+       
+
       <Link className="link" to="appointment">Agendamento</Link>
       <Link className="link" to="activities">Atividades</Link>
       <Link className="link" to="about">Sobre</Link>
       <Link className="link" to="plans">Planos</Link>
       </nav>
+      <ul className="dropdown">
+      <Link className="link" to="appointment">Agendamento</Link>
+      <Link className="link" to="activities">Atividades</Link>
+      <Link className="link" to="about">Sobre</Link>
+      <Link className="link" to="plans">Planos</Link>
+
+ </ul>
     </header>
   )
 }
